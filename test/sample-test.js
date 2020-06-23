c5d56225a31ca4d2b6410e1f6d65e76bc04f8a0b
Daniel Nolan <DanNolan99@gmail.com>
2020-06-23 08:56:35 -0400

    adds tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,14 +1,47 @@
-const { expect } = require("chai");
+const { formatEther, parseEther } = ethers.utils;
+const deposit = parseEther("1");
+const { assert } = require("chai");
 
-describe("Greeter", function() {
-  it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    
-    await greeter.deployed();
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Escrow", function () {
+    let depositor;
+    let arbiter;
+    let beneficiary;
+    beforeEach(async () => {
+        [depositor, arbiter, beneficiary] = await ethers.provider.listAccounts();
+        const Escrow = await ethers.getContractFactory("Escrow");
+        contract = await Escrow.deploy(arbiter, beneficiary, {
+            value: deposit
+        });
+        await contract.deployed();
+    });
 
-    await greeter.setGreeting("Hola, mundo!");
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+    it("should be funded", async () => {
+        let balance = await ethers.provider.getBalance(contract.address);
+        assert.equal(balance.toString(), deposit.toString());
+    });
+
+    it("should default the isApproved state to false", async () => {
+        const isApproved = await contract.isApproved();
+        assert(!isApproved, "Expected isApproved to be false!");
+    });
+
+    describe("after approval from the arbiter", () => {
+        let before;
+        let tx;
+        beforeEach(async () => {
+            before = await ethers.provider.getBalance(beneficiary);
+            const signer = await ethers.provider.getSigner(arbiter);
+            tx = await contract.connect(signer).approve();
+        });
+
+        it("should transfer balance to beneficiary", async () => {
+            const after = await ethers.provider.getBalance(beneficiary);
+            assert.equal(after.sub(before).toString(), deposit.toString());
+        });
+
+        it("should set the isApproved state to true", async () => {
+            const isApproved = await contract.isApproved();
+            assert(isApproved, "Expected isApproved to be true!");
+        });
+    });
 });
